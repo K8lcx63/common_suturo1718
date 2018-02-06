9e7b83b5f561c4edab50332c02631885e568f401
Unknown <mbertram@uni-bremen.de>
2018-02-06 13:20:38 +0100

    add targets + add include

--- marker_publisher/include/marker_publisher/marker_publisher.h
+++ marker_publisher/include/marker_publisher/marker_publisher.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <visualization_msgs/Marker.h>
 #include <geometry_msgs/PointStamped.h>
+#include <ros/ros.h>
 
 enum class Color {BLACK, WHITE, BROWN, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE};
 
